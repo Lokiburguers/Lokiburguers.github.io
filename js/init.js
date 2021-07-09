9e41b1c14b0bdad2c1dea2a8d0520a7c29cd91b1
Lokiburguers <87153902+Lokiburguers@users.noreply.github.com>
2021-07-08 20:51:58 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,11 +3,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyCAliDGF23jRYfGiPcbL_5xk5DUw3TRh7Y",
+      apiKey: "AIzaSyCAliDGF23jRYfGiPcbL_5xk5DUw3TRh7Y",
     authDomain: "autenfi41-b4af7.firebaseapp.com",
     projectId: "autenfi41-b4af7",
     storageBucket: "autenfi41-b4af7.appspot.com",
     messagingSenderId: "1026956636891",
     appId: "1:1026956636891:web:1943f2b2a33e5b0e6d12fe",
     measurementId: "G-RFEBDWVFEQ"
+  
   )};
